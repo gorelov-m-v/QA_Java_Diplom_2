d2c979ddc1b60d9d85c3bcd6f73d8e31ff2b3107
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-03-08 18:57:12 +0300

    Переработал генератор данных, задал зависимость генерируемых данных от ограничений поля.

---
+++ src/main/java/Util/GeneratorConfigs.java
@@ -0,0 +1,12 @@
+package Util;
+
+public class GeneratorConfigs {
+
+    protected static int emailMaxLength = 50;
+
+    protected static int passwordMinLength = 8;
+    protected static int passwordMaxLength = 50;
+
+    protected static int nameMinLength = 2;
+    protected static int nameMaxLength = 50;
+}
